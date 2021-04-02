e2abeb9266fd0e058e5fd5269ffb7256aad29a42
Gabriel H. S. Pires <gabrielhpires@gmail.com>
2021-04-02 19:58:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,3 @@ sequences = utils.readSequences(sys.argv[1])
 nw.nw(sequences[0], sequences[1])
 
 # Run with 'python3 main.py Sequences/input3'
-# Checking correctness with gtuckerkellogg.github.io/pairwise/demo/
